29af7a6df8399bd3c5c02b8483a4220f0fcb4b7f
RexDeux <nm.cdc@hotmail.com>
2022-02-03 08:51:04 +0000

    fixing classes

--- cal.py
+++ cal.py
@@ -9,11 +9,8 @@ from flask import Flask, json, render_template, jsonify, request
 t = time()
 
 
-class Calculations():
-    with open('app.json') as f:
-            d = json.load(f)
-            print(d)
-    def __init__(self, cart_value,delivery_distance, num_items, time ,delivery_fee):
+class Calculations:
+    def __init__(self, cart_value=None,delivery_distance=None, num_items=None, time=None ,delivery_fee=None):
         self.cart_value = cart_value
         self.delivery_distance = delivery_distance
         self.num_items = num_items
@@ -21,20 +18,10 @@ class Calculations():
         self.delivery_fee = delivery_fee
         
     #Dan = Calculations(790, 2235, 4, "2021-10-12T13:00:00Z", {delivery_fee})
-    def calc(self, param):
-            num_items = int(param['number_of_items'])
-            delivery_distance = int(param['delivery_distance'])
-            cart_value = int(param['cart_value'])
-            ti = param['time']
-            Friday_rush = calendar.FRIDAY
-            start = '15:00:00'
-            enda = '19:00:00'
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
+    def calc(self, cart_value=None,delivery_distance=None, num_items=None, time=None ,delivery_fee=None):
             #delivery_fee = 0
             # calculations
             surcharge = 10 - cart_value if cart_value < 10 else 0
-            base_fee = 2
             delivery_distance_fee = math.ceil(
                 delivery_distance / 500) if delivery_distance > 500 else 1
             surcharge = 0.50
@@ -53,9 +40,21 @@ class Calculations():
                 delivery_fee = 15
             return delivery_fee
 
+with open('app.json') as f:
+        d = json.load(f)
+        print(d)
+        num_items = ['number_of_items']
+        delivery_distance = ['delivery_distance']
+        cart_value = ['cart_value']
+        t = ['time']
+        Friday_rush = calendar.FRIDAY
+        start = '15:00:00'
+        enda = '19:00:00'
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
 
+print(Calculations.calc(cart_value, delivery_distance, num_items, t))
 
-print("Math = ", Calculations.delivery_fee)
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
