768451b19cd066559f647e64162561bd0e1633a5
RexDeux <nm.cdc@hotmail.com>
2022-02-03 09:22:05 +0000

    for in every push we inch closer to victory

--- cal.py
+++ cal.py
@@ -3,6 +3,7 @@ from datetime import datetime, time
 import calendar
 import json
 from flask import Flask, json, render_template, jsonify, request
+from numpy import integer
 
 #from flask_restful import Resource
 
@@ -10,6 +11,19 @@ t = time()
 
 
 class Calculations:
+    with open('app.json') as f:
+            d = json.load(f)
+            print(d)
+            num_items = ['number_of_items']
+            delivery_distance = ['delivery_distance']
+            cart_value = ['cart_value']
+            t = ['time']
+            Friday_rush = calendar.FRIDAY
+            start = '15:00:00'
+            enda = '19:00:00'
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+
     def __init__(self, cart_value=None,delivery_distance=None, num_items=None, time=None ,delivery_fee=None):
         self.cart_value = cart_value
         self.delivery_distance = delivery_distance
@@ -18,14 +32,12 @@ class Calculations:
         self.delivery_fee = delivery_fee
         
     #Dan = Calculations(790, 2235, 4, "2021-10-12T13:00:00Z", {delivery_fee})
-    def calc(self, cart_value=None,delivery_distance=None, num_items=None, time=None ,delivery_fee=None):
-            #delivery_fee = 0
+    def calc(cart_value=None,delivery_distance=None, num_items=None,delivery_fee=None):
             # calculations
             surcharge = 10 - cart_value if cart_value < 10 else 0
             delivery_distance_fee = math.ceil(
                 delivery_distance / 500) if delivery_distance > 500 else 1
-            surcharge = 0.50
-            surcharge = 0 if cart_value <= 4 else 0.50 * num_items
+            surcharge = int( 0 if cart_value <= 4 else 0.50 * num_items)
             delivery_fee = delivery_distance_fee + surcharge
             if delivery_fee > 15:
                 delivery_fee = 15
@@ -40,20 +52,8 @@ class Calculations:
                 delivery_fee = 15
             return delivery_fee
 
-with open('app.json') as f:
-        d = json.load(f)
-        print(d)
-        num_items = ['number_of_items']
-        delivery_distance = ['delivery_distance']
-        cart_value = ['cart_value']
-        t = ['time']
-        Friday_rush = calendar.FRIDAY
-        start = '15:00:00'
-        enda = '19:00:00'
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
 
-print(Calculations.calc(cart_value, delivery_distance, num_items, t))
+#print(Calculations.calc(cart_value, delivery_distance, num_items, t))
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -79,6 +79,7 @@ def home():
         #result = obj.delivery_fee
         return jsonify(
             {'data': d},
+            {'delivery fee': "closer to victory"}
             #{'result': result}
         )
 
