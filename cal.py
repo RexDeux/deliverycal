a0009ca0e6a1c58b82e3e4807d290ef5988e3f2b
RexDeux <nm.cdc@hotmail.com>
2022-02-03 21:57:53 +0000

    http api running, mixing happyhour

--- cal.py
+++ cal.py
@@ -5,22 +5,25 @@ import dateutil.relativedelta as REL
 import json
 from flask import Flask, json, render_template, jsonify, request
 from numpy import integer
+import time
 
-#from flask_restful import Resource
+
 class Calculations():
     with open('app.json') as f:
             d = json.load(f)
-            print(d)
+            #print(d)
             num_items = ['number_of_items']
             delivery_distance = ['delivery_distance']
             cart_value = ['cart_value']
             time = ['time']
+            date_string = str(time)
+            #print (datetime.fromisoformat(date_string))
 
     def __init__(self, cart_value,delivery_distance, num_items, time):
         self.cart_value = cart_value
         self.delivery_distance = delivery_distance
         self.num_items = num_items
-        self.time = datetime.strptime(time, '%b %d %Y %I:%M%p')
+        self.time = time
         
     def surcharge(self):
         b = (10 - self.cart_value if self.cart_value < 10 else 0)
@@ -48,35 +51,34 @@ class Calculations():
             elif self.time > "T19:00:00Z":
                 delivery_fee * 1
         return(delivery_fee)
+app = Flask(__name__)
+app.config["DEBUG"] = True
 
-demo1 = Calculations(790, 2235, 4, "2022-02-04T16:00:00Z")
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+        with open('app.json') as f:
+            d = json.load(f)
+        demo1 = Calculations(790, 2235, 4, "2022-02-04T16:00:00Z")
+        return jsonify(
+            {'data': d},
+            {'delivery fee': "closer to victory"},
+            {'Your surcharge is': demo1.surcharge()},
+            {'Your delivery distance fee is': demo1.delivery_distance_fee()}
+        )
+        
+
+@app.route('/<int:num>', methods = ['GET'])
+def disp(num):
+    return jsonify({'data': num**2})
+
+
+if __name__ == '__main__':
+    
+    app.run(debug = True)
+    app.run()
+
 print('Your surcharge is:',demo1.surcharge(),'euros')
 print('Your delivery distance fee is:',demo1.delivery_distance_fee(),'euros')
 #print('Your total fee is:',demo1.surcharge() + demo1.delivery_distance_fee())
 print('Your delivery fee is', demo1.friday_rush())
-
-#app = Flask(__name__)
-#app.config["DEBUG"] = True
-
-#@app.route('/', methods = ['GET', 'POST'])
-#def home():
-    #if(request.method == 'GET'):
-        #with open('app.json') as f:
-            #d = json.load(f)
-        #data = request.json()
-        #result = obj.delivery_fee
-        #return jsonify(
-            #{'data': d},
-            #{'delivery fee': "closer to victory"},
-            #{'result': Calculations.cart_value}
-        #)
-        
-
-#@app.route('/<int:num>', methods = ['GET'])
-#def disp(num):
-    
-    #return jsonify({'data': num**2})
-
-#if __name__ == '__main__':
-    
-    #app.run(debug = True)
