cb45d31e12324e2c68d014e12870cf03b62540b2
RexDeux <nm.cdc@hotmail.com>
2022-02-03 16:56:50 +0000

    closer

--- cal.py
+++ cal.py
@@ -18,11 +18,6 @@ class Calculations:
             delivery_distance = ['delivery_distance']
             cart_value = ['cart_value']
             t = ['time']
-            Friday_rush = calendar.FRIDAY
-            start = '15:00:00'
-            enda = '19:00:00'
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
 
     def __init__(self, cart_value=None,delivery_distance=None, num_items=None, time=None ,delivery_fee=None):
         self.cart_value = cart_value
@@ -31,28 +26,33 @@ class Calculations:
         self.time = time
         self.delivery_fee = delivery_fee
         
-    #Dan = Calculations(790, 2235, 4, "2021-10-12T13:00:00Z", {delivery_fee})
-    def calc(cart_value=None,delivery_distance=None, num_items=None,delivery_fee=None):
+    #Dan = Calculations(790, 2235, 4, "2021-10-12T13:00:00Z", {self.delivery_fee})
+    def calc(self):
             # calculations
-            surcharge = 10 - cart_value if cart_value < 10 else 0
+            surcharge = 10 - self.cart_value if self.cart_value < 10 else 0
             delivery_distance_fee = math.ceil(
-                delivery_distance / 500) if delivery_distance > 500 else 1
-            surcharge = int( 0 if cart_value <= 4 else 0.50 * num_items)
-            delivery_fee = delivery_distance_fee + surcharge
-            if delivery_fee > 15:
-                delivery_fee = 15
-            elif cart_value >= 100:
-                delivery_fee = 0
+                self.delivery_distance / 500) if self.delivery_distance > 500 else 1
+            surcharge = int( 0 if self.cart_value <= 4 else 0.50 * self.num_items)
+            self.delivery_fee = delivery_distance_fee + surcharge
+            if self.delivery_fee > 15:
+                self.delivery_fee = 15
+            elif self.cart_value >= 100:
+                self.delivery_fee = 0
 
+            Friday_rush = calendar.FRIDAY
+            start = '15:00:00'
+            enda = '19:00:00'
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            
             while (Friday_rush and current_time >= start):
-                delivery_fee = delivery_fee * 1.1
+                self.delivery_fee = self.delivery_fee * 1.1
             if current_time <= enda:
-                delivery_fee = delivery_fee
-            elif delivery_fee > 15:
-                delivery_fee = 15
-            return delivery_fee
-
-
+                self.delivery_fee = self.delivery_fee
+            elif self.delivery_fee > 15:
+                self.delivery_fee = 15
+            return self.delivery_fee
+            
 #print(Calculations.calc(cart_value, delivery_distance, num_items, t))
 
 app = Flask(__name__)
@@ -64,11 +64,11 @@ def home():
         with open('app.json') as f:
             d = json.load(f)
         #data = request.json()
-        #result = obj.delivery_fee
+        #result = obj.self.delivery_fee
         return jsonify(
             {'data': d},
-            {'delivery fee': "closer to victory"}
-            #{'result': result}
+            {'delivery fee': "closer to victory"},
+            {'result': Calculations.delivery_fee}
         )
 
 @app.route('/<int:num>', methods = ['GET'])
