97d1b6ffc0d6c742e52e6120a466a6171821a40c
RexDeux <nm.cdc@hotmail.com>
2022-02-03 17:22:14 +0000

    reorganizing everything

--- cal.py
+++ cal.py
@@ -27,55 +27,64 @@ class Calculations:
         self.delivery_fee = delivery_fee
         
     #Dan = Calculations(790, 2235, 4, "2021-10-12T13:00:00Z", {self.delivery_fee})
-    def calc(self):
-            # calculations
-            surcharge = 10 - self.cart_value if self.cart_value < 10 else 0
-            delivery_distance_fee = math.ceil(
-                self.delivery_distance / 500) if self.delivery_distance > 500 else 1
-            surcharge = int( 0 if self.cart_value <= 4 else 0.50 * self.num_items)
-            self.delivery_fee = delivery_distance_fee + surcharge
-            if self.delivery_fee > 15:
-                self.delivery_fee = 15
-            elif self.cart_value >= 100:
-                self.delivery_fee = 0
-
-            Friday_rush = calendar.FRIDAY
-            start = '15:00:00'
-            enda = '19:00:00'
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
+    def surcharge(self):
+        self.surcharge = 10 - self.cart_value if self.cart_value < 10 else 0
+        return(self.surcharge)
+    
+    def delivery_distance_fee(self):
+        self.delivery_distance_fee = math.ceil(
+            self.delivery_distance / 500) if self.delivery_distance > 500 else 1
+        surcharge = int( 0 if self.cart_value <= 4 else 0.50 * self.num_items)
+        self.delivery_fee = self.delivery_distance_fee + surcharge
+        if self.delivery_fee > 15:
+            self.delivery_fee = 15
+        elif self.cart_value >= 100:
+            self.delivery_fee = 0
+        return(self.surcharge + self.delivery_distance_fee)
+        
+    def happyhour(self):
+        # calculations
+        #surcharge = 10 - self.cart_value if self.cart_value < 10 else 0
+        Friday_rush = calendar.FRIDAY
+        start = '15:00:00'
+        enda = '19:00:00'
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
             
-            while (Friday_rush and current_time >= start):
-                self.delivery_fee = self.delivery_fee * 1.1
-            if current_time <= enda:
-                self.delivery_fee = self.delivery_fee
-            elif self.delivery_fee > 15:
-                self.delivery_fee = 15
-            return self.delivery_fee
-            
-#print(Calculations.calc(cart_value, delivery_distance, num_items, t))
+        while (Friday_rush and current_time >= start):
+            self.delivery_fee = self.delivery_fee * 1.1
+        if current_time <= enda:
+            self.delivery_fee = self.delivery_fee
+        elif self.delivery_fee > 15:
+            self.delivery_fee = 15 
+        return(self.delivery_fee)
 
-app = Flask(__name__)
-app.config["DEBUG"] = True
+demo1 = Calculations(790, 2235, 4, "2021-10-12T13:00:00Z",)
+print(demo1.surcharge())
+print(demo1.delivery_distance_fee())
+print(demo1.happyhour())
+#app = Flask(__name__)
+#app.config["DEBUG"] = True
 
-@app.route('/', methods = ['GET', 'POST'])
-def home():
-    if(request.method == 'GET'):
-        with open('app.json') as f:
-            d = json.load(f)
+#@app.route('/', methods = ['GET', 'POST'])
+#def home():
+    #if(request.method == 'GET'):
+        #with open('app.json') as f:
+            #d = json.load(f)
         #data = request.json()
         #result = obj.self.delivery_fee
-        return jsonify(
-            {'data': d},
-            {'delivery fee': "closer to victory"},
-            {'result': Calculations.delivery_fee}
-        )
+        #return jsonify(
+            #{'data': d},
+            #{'delivery fee': "closer to victory"},
+            #{'result': Calculations.cart_value}
+        #)
+        
 
-@app.route('/<int:num>', methods = ['GET'])
-def disp(num):
+#@app.route('/<int:num>', methods = ['GET'])
+#def disp(num):
     
-    return jsonify({'data': num**2})
+    #return jsonify({'data': num**2})
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     
-    app.run(debug = True)
+    #app.run(debug = True)
